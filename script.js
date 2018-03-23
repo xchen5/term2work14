d678067d8c77d6ca2dae2bc776a058a2a9274e79
xchen5 <xchen5@stuy.edu>
2018-03-23 14:06:34 -0400

    stuff

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 var revenue2008 = [911, 308, 68.1, 108, 1160];
 var revenue2016 = [986, 262, 86.7, 195, 1460];
+var chart = d3.select(".chart");
+var bar = chart.selectAll("div");
+var barUpdate = bar.data(revenue2008);
+var barEnter = barUpdate.enter().append("div");
